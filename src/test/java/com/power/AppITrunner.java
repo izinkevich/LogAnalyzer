2c145dbc0c0e1c5347e7b9995b4d606fdd27b866
zinkevichid <ilya.zinkevich@gmail.com>
2017-03-02 12:49:10 +0300

    adapt integration test to container size changes

--- src/test/java/com/power/AppITrunner.java
+++ src/test/java/com/power/AppITrunner.java
@@ -43,8 +43,7 @@ public class AppITrunner {
 		new App(logTempFile, new PrintStream(outputFile));
 
 		waitFor(IntrusionConstants.DELAY_BETWEEN_FILE_READS_MILLIS / 2);
-		Assert.assertEquals(FAILURES_INIT_COUNT - IntrusionConstants.MAX_NUM_OF_ENTRIES_STORED + 1,
-				Files.readLines(outputFile, Charsets.UTF_8).size());
+		Assert.assertEquals(FAILURES_INIT_COUNT - IntrusionConstants.MAX_NUM_OF_ENTRIES_STORED + 1, Files.readLines(outputFile, Charsets.UTF_8).size());
 
 		System.out.println("Append 2 failure lines and 1 success line to log");
 		Files.append(logErrEntry1, logTempFile, Charsets.UTF_8);
@@ -53,8 +52,16 @@ public class AppITrunner {
 
 		waitFor(IntrusionConstants.DELAY_BETWEEN_FILE_READS_MILLIS);
 		System.out.println("Check 2 more alarm messages in output");
-		Assert.assertEquals(FAILURES_INIT_COUNT - IntrusionConstants.MAX_NUM_OF_ENTRIES_STORED + 1 + 2,
-				Files.readLines(outputFile, Charsets.UTF_8).size());
+		Assert.assertEquals(FAILURES_INIT_COUNT - IntrusionConstants.MAX_NUM_OF_ENTRIES_STORED + 1 + 2, Files.readLines(outputFile, Charsets.UTF_8).size());
+
+		System.out.println("Append " + IntrusionConstants.MAX_NUM_OF_ENTRIES_STORED + " more FAILURE messages after more than 5 minutes to log");
+		for (int i = 1; i <= IntrusionConstants.MAX_NUM_OF_ENTRIES_STORED; i++) {
+			Files.append(addTime(logErrEntry2, IntrusionConstants.TIME_TO_STORE_MILLIS + i * 1000), logTempFile, Charsets.UTF_8);
+		}
+
+		waitFor(IntrusionConstants.DELAY_BETWEEN_FILE_READS_MILLIS);
+		System.out.println("Check  - only one new alarm message is expected in output");
+		Assert.assertEquals(FAILURES_INIT_COUNT - IntrusionConstants.MAX_NUM_OF_ENTRIES_STORED + 1 + 3, Files.readLines(outputFile, Charsets.UTF_8).size());
 
 		System.out.println("Integration test PASSED, stop the application");
 	}
@@ -66,6 +73,13 @@ public class AppITrunner {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static String addTime(String entry, long millis) {
+		String[] parts = entry.split(",");
+		Long newTime = Long.parseLong(parts[1]) + millis;
+		parts[1] = newTime.toString();
+		return parts[0] + ',' + parts[1] + ',' + parts[2] + ',' + parts[3];
 	}
 
 }
